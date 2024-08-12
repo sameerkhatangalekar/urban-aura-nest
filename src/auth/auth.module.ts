b237567848b565e84d7a6ff310fc51b2c818dcbe
sameerkhatangalekar <sameerkhatangalekar@gmail.com>
2024-08-13 03:52:23 +0530

    Code cleanup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,17 +5,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    /**
-     ** This register configuratio is global, if require this can be configured directly while using JwtService's sign function to configure different secret for refresh token and access token
-     */
-    JwtModule.register({
-      secret: new ConfigService().get('JWT_SECRET'),
-      signOptions: {
-        expiresIn: 60 * 60 * 8,
-      },
-    }),
-  ],
+  imports: [],
   controllers: [AuthController],
   providers: [AuthService],
 })
